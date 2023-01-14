15d1b6b92363e8903b0e0e55a5cd104a704e81ff
ulugbekpy <ulugbekpycpp@gmail.com>
2023-01-15 01:23:18 +0500

    CRUD functions added.Some bugs are fixed and some codes optimized

--- app/urls.py
+++ app/urls.py
@@ -1,12 +1,18 @@
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
-from .views import mainpage,PostListView,PostDetailView,PostUpdateView,PostCreateView
+from .views import (mainpage,
+                    PostListView,
+                    PostDetailView,
+                    PostUpdateView,
+                    PostCreateView,
+                    PostDeleteView)
 
 urlpatterns = [
     path('',mainpage,name='main-page'),
+    path('post/new/',PostCreateView.as_view(template_name = "app/post_create.html"),name='post-create'),
     path('posts/',PostListView.as_view(),name='posts'),
+    path('post/<int:pk>/update/',PostUpdateView.as_view(template_name = "app/post_update.html"),name='post-update'),
     path('post/<int:pk>/',PostDetailView.as_view(),name='post-detail'),
-    path('post/new/',PostCreateView.as_view(),name='post-create'),
-    path('post/<int:pk>/',PostUpdateView.as_view(),name='post-update'),
+    path('post/<int:pk>/delete/',PostDeleteView.as_view(),name='post-delete'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- app/views.py
+++ app/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
-from django.contrib.auth.mixins import LoginRequiredMixin
-from django.views.generic import ListView,DetailView,CreateView,UpdateView
+from django.contrib.auth.mixins import LoginRequiredMixin,UserPassesTestMixin
+from django.views.generic import (ListView,
+                                  DetailView,
+                                  CreateView,
+                                  UpdateView,
+                                  DeleteView)
 from .models import Post
 
 # This view is for main page
@@ -17,16 +21,6 @@ class PostListView(ListView):
     ordering = ['-date_posted']
 
 
-class PostDetailView(DetailView):
-    model = Post
-    template_name = 'app/post-detail.html'
-
-
-class PostUpdateView(UpdateView):
-    model = Post
-    template_name = 'app/post-detail.html'
-
-
 class PostCreateView(LoginRequiredMixin,CreateView):
     model = Post
     fields = ['title','content']
@@ -34,3 +28,33 @@ class PostCreateView(LoginRequiredMixin,CreateView):
     def form_valid(self, form):
         form.instance.author = self.request.user
         return super().form_valid(form)
+
+
+class PostDetailView(DetailView):
+    model = Post
+
+
+class PostUpdateView(LoginRequiredMixin,UserPassesTestMixin,UpdateView):
+    model = Post
+    fields = ['title','content']
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        return False
+
+
+class PostDeleteView(LoginRequiredMixin,UserPassesTestMixin,DeleteView):
+    model = Post
+    success_url = '/posts/'
+
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        return False
